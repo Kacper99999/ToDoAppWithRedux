b0a888c6f0357ec239ce3b3d3538f63decee60d9
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-09-07 13:33:56 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+=======
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+>>>>>>> 7e1c700 (Initial commit)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
 })
+=======
+  base: "/ToDoAppWithRedux/",
+  build: {
+    outDir: "./dist",
+  },
+});
+>>>>>>> 7e1c700 (Initial commit)
